90c1d6fcf629d2c65257642879b4f390ab593cee
rpsmith77 <44273059+rpsmith77@users.noreply.github.com>
2021-10-29 17:39:25 -0400

    test nn w/ app engine

--- praw_test.py
+++ praw_test.py
@@ -20,12 +20,12 @@ def main(subreddit_name: str) -> dict:
 
     hot_subreddit = subreddit.hot()
     x = {}
-    # hate_network = na.NeuralAnalyzer("Neural/neural_networks/hate_neural/hate_model",
-    #                                  "Neural/neural_networks/hate_neural/tokenizer.pickle")
+    hate_network = na.NeuralAnalyzer("Neural/neural_networks/hate_neural/hate_model",
+                                     "Neural/neural_networks/hate_neural/tokenizer.pickle")
     for submission in hot_subreddit:
-        # predicted_value = hate_network.predict_text([submission.title])
-        x.update({submission.title: random.random()})  # random represents test value for nlp
-        # x.update({submission.title: predicted_value})
+        predicted_value = hate_network.predict_text([submission.title])
+        # x.update({submission.title: random.random()})  # random represents test value for nlp
+        x.update({submission.title: predicted_value})
 
     return x
 
